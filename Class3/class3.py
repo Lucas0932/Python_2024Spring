75a42f150e0bc90e8953f8ae97eb8e5b5e82a924
Lucas0932 <LucasChang1112@gmail.com>
2024-03-10 18:12:43 +0800

    class 3 notes

---
+++ Class3/class3.py
@@ -0,0 +1,40 @@
+# make a new class
+class Car: 
+    def __init__(self, color):
+        self.color = color
+        self.next = None
+# traverse the list
+def traverse(head):
+    ptr = head
+    while True:
+        print("The color of the car is {}.".format(ptr.color))
+        if ptr.next == head:
+            break
+        ptr = ptr.next
+    print("finish traverse!")
+
+# make the first element
+head = Car("red")
+ptr = head
+ptr.next = None
+# add new element
+new_data = Car("blue")
+ptr.next = new_data
+new_data.next = head
+# before adding a new one, make it be the head
+new = Car("black")
+new.next = head
+ptr = head
+while ptr.next != head:
+    ptr = ptr.next
+ptr.next = new
+# insert new one into middle
+head = new
+new = Car("pink")
+ptr = head
+new.next = ptr.next
+while ptr.color != "red":
+    ptr = ptr.next
+new.next = ptr.next
+ptr.next = new
+traverse(head)
